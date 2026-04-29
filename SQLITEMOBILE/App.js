cd675a77aba8b35bb747779240540ca31b296220
Emerson Rocha <emerson_oliver_rocha@hotmail.com>
2026-04-29 12:24:17 -0300

    delete, selectId, selectNome

--- SQLITEMOBILE/App.js
+++ SQLITEMOBILE/App.js
@@ -1,31 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import {
-    Banco, createTable, insertUsuario, selectUsuarios, selectUsuarioById, selectUsuarioNome
-    , deleteUsuario, updateUsuario
+  Banco, createTable, insertUsuario, selectUsuarios, selectUsuarioById, selectUsuarioNome
+  , deleteUsuario, updateUsuario
 } from './Banco/Config';
 
-import {useEffect} from 'react';
+import { useEffect } from 'react';
 
 export default function App() {
 
-    useEffect(()=>{
-     async function Main (){
-       const db = await Banco();
-       await createTable(db); 
+  useEffect(() => {
+    async function Main() {
+      const db = await Banco();
+      await createTable(db);
       //await insertUsuario(db, " sofie ", "xcps.com.br")  ;
       // exibir campos
-          const resp =  await selectUsuarios(db) ;
-          console.log(resp) ;
+      // const resp =  await selectUsuarios(db) ;
+      // console.log(resp) ;
 
-     };
-     Main() ;
-    },[])
+      // exibir por id
+        // let idCampo =  await selectUsuarioById(db,3);
+        // console.log(idCampo) ;
+
+        // exibe por nome
+        // let nomeCampo =  await selectUsuarioNome(db, " Zelão ");
+        // console.log(nomeCampo) ;
+
+        // exibe por delete
+        let delCampo =  await deleteUsuario(db, 3);
+        console.log(delCampo)  ;
+
+    };
+    Main();
+  }, [])
 
 
   return (
     <View style={styles.container}>
-      
+
     </View>
   );
 }
